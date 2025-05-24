5e2332fa2c4cc8d2d879b4be45d934ef17994dac
Vansh Garg <vanshgarg4548@gmail.com>
2025-05-24 20:39:39 +0530

    Time: 2 ms (52.59%) | Memory: 44.8 MB (89.00%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+
+    public List<Integer> findWordsContaining(String[] words, char targetChar) {
+
+        List<Integer> indicesWithTargetChar = new ArrayList<>();
+      
+        for (int index = 0; index < words.length; ++index) {
+
+            if (words[index].indexOf(targetChar) != -1) {
+                indicesWithTargetChar.add(index);
+            }
+        }
+  
+        return indicesWithTargetChar;
+    }
+}
